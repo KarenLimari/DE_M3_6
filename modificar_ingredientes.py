d1c826d35b252f2f4acce093a628659a65180b5e
Karen Limari <karenlimari20@gmail.com>
2024-08-15 12:58:12 +0200

    Agregar funcion modificacion_ingredientes

--- modificar_ingredientes.py
+++ modificar_ingredientes.py
@@ -3,7 +3,7 @@ def modificar_ingredientes(ingredientes):
         print("\n¿Quieres modificar o agregar algún ingrediente?")
         print("1.Agregar ingrediente")
         print("2.Eliminar ingrediente")
-        print("No quiero modificar ni agregar nada")
+        print("3.No quiero modificar ni agregar nada")
 
         eleccion = input("Ingrese el número de tu elección: ")
 
@@ -12,10 +12,35 @@ def modificar_ingredientes(ingredientes):
             print(
                 "Tomate, Champiñones, Aceituna, Cebolla, Pollo, Jamón, Carne, Tocino, Queso"
             )
-            nuevo_ingrediente = input("Ingresa el ingrediente que deseas agregar: ")
+            nuevo_ingrediente = input(
+                "Ingresa el ingrediente que deseas agregar: "
+            ).capitalize()
 
             if nuevo_ingrediente in ingredientes:
                 print(f"{nuevo_ingrediente} ya está en la pizza.")
             else:
                 ingredientes.append(nuevo_ingrediente)
                 print(f"{nuevo_ingrediente} agregado a la pizza.")
+
+        elif eleccion == "2":
+            print("\nIngredientes actuales:", ingredientes)
+            eliminar_ingrediente = input(
+                "Ingresa ingrediente que deseas eliminar: "
+            ).capitalize()
+            if eliminar_ingrediente in ingredientes:
+                ingredientes.remove(eliminar_ingrediente)
+                print(f"{eliminar_ingrediente} ha sido eliminado de la pizza")
+            else:
+                print(f"{eliminar_ingrediente} no está en la pizza")
+
+        elif eleccion == "3":
+            break
+
+        else:
+            print("Opción no váida. Intenta de nuevo")
+
+            return ingredientes
+
+
+ingredientes = ["Queso"]
+modificar_ingredientes(ingredientes)

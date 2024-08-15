8e29c015be7f82cce3d11f3b1b2f91b8be025675
Karen Limari <karenlimari20@gmail.com>
2024-08-15 02:58:21 +0200

    first commit

---
+++ pizzajat.py
@@ -0,0 +1,7 @@
+def elegir_masa():
+  print("\nElige el tipo de masa que quieres para tu pizza: ")
+  print("1.Masa tradicional")
+  print("2.Masa delgada")
+  print("3.Masa con bordes de queso")
+
+elegir_masa()

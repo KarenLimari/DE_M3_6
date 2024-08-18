0a43462868f69ed7dff0decc3d12dc2f4bbe5ec5
Karen Limari <karenlimari20@gmail.com>
2024-08-18 16:50:42 +0200

    Agregar docstring función calcular_tiempo y comentarios

--- calcular_tiempo.py
+++ calcular_tiempo.py
@@ -1,4 +1,17 @@
 def calcular_tiempo(ingredientes):
-    tiempo_base = 20
-    tiempo_total = tiempo_base + 2 * len(ingredientes)
+    """
+    Permite calcular el tiempo de preparación de la pizza basado en la cantidad de ingredientes.
+    Parámetros:
+    -----------
+    ingredientes: list
+        Lista de ingredeintes de la pizza. Cada ingrediente adicional suma 2 minutos.
+    Returns:
+    --------
+    int
+        El tiempo total estimado en minutos de preparación de la pizza.
+    """
+    tiempo_base = 20  # tiempo base en minutos.
+    tiempo_total = tiempo_base + 2 * len(
+        ingredientes
+    )  # Cada ingrediente adicional suma 2 minutos.
     return tiempo_total
